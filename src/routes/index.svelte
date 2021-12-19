c07c6f90701ebfc5e22e4dd58a2abc5cf029d9b9
wilsonyeoh123 <wilsonyeohexpertise@gmail.com>
2021-12-19 02:45:40 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    let location="";
+    let temperature="";
+    let description="";
+</script>
+<h1>Weather App</h1>
+
+<form on:submit|preventDefault={getWeather}>
+    <label for="location">Location:</label>
+    <input id="location" bind:value={location} type="text" />
+    <button type="submit">Check</button>
+</form>
+
