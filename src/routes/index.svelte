8b914fe1cfca1a35c50223dc103ff47120fa508e
wilsonyeoh123 <wilsonyeohexpertise@gmail.com>
2021-12-19 03:05:33 +0000

    commit 2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,17 @@
     let location="";
     let temperature="";
     let description="";
+    
+async function getWeather() {
+    fetch('https://goweather.herokuapp.com/weather/' + location)
+    .then((response) => response.json())
+    .then((data) => {
+  	temperature = data.temperature; // Path for temperature
+  	description = data.description; // Path for description
+	});
+}
+
+
 </script>
 <h1>Weather App</h1>
 
@@ -10,4 +21,6 @@
     <input id="location" bind:value={location} type="text" />
     <button type="submit">Check</button>
 </form>
+<p>Temperature: {temperature}</p>
+<p>Description: {description}</p>
 
